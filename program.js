aee659414a7a417baf0c4ee11615eda9a4b34cec
Bhawna <bhawna.bhatt@gmail.com>
2024-09-08 08:12:14 +0530

    9/8/24

---
+++ program.js
@@ -0,0 +1,51 @@
+
+/*Keyboard shorcuts 
+
+shif + u/d
+cmd|ctrl + r/l
+option|alt  + r/l
+shift + option|alt  + r/l
+shift + cmd|ctrl  + r/l
+opt|alt + u/d
+shif + opt|alt + u/d
+
+highlight something(say log) and then cmd|crtl +D --> will select next log and we can edit all log
+or if we need to select all of them all at once cmd|ctrl + shift + L AND we can edit all
+--- keep cursor somewhere and then alt|opt +click somewhere else -- where we click gets selected and
+
+
+
+*/
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+
+console.warn("hello");
+console.log("hello");
+
+console.error("hello");
+
+console.table
+
+
+console.group('somple')
+console.log("hello");
+
+console.warn("hello");
+
+console.error("hello");
+
+console.table({greetijng : "hello"});
+console.groupEnd()
+
+/*Keyboard shorcuts 
+
+shif + u/d
+cmd|ctrl + r/l
+option|alt  + r/l
+shift + option|alt  + r/l
+shift + cmd|ctrl  + r/l
+opt|alt + u/d
+
+*/
